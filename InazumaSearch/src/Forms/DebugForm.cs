c165cb0c1e2a2c2dcb0a67ab7394f91e316b6413
Dice <tetradice@gmail.com>
2020-07-17 08:46:13 +0900

    自動更新組み込みテスト

--- InazumaSearch/src/Forms/DebugForm.cs
+++ InazumaSearch/src/Forms/DebugForm.cs
@@ -126,6 +126,10 @@ namespace InazumaSearch.Forms
                 var size = new FileInfo(fullPath).Length;
                 LsvObjectList.Items.Add(new ListViewItem(new string[] { objPair.Key, Util.FormatFileSizeByMB(size) }));
             }
+
+            AutoUpdater.CheckForUpdateEvent += AutoUpdaterOnCheckForUpdateEvent;
+            AutoUpdater.ShowRemindLaterButton = false;
+            AutoUpdater.PersistenceProvider = new JsonFilePersistenceProvider("autoupdate.json");
         }
 
         private void BtnGetExcelText_Click(object sender, EventArgs e)
@@ -253,6 +257,76 @@ namespace InazumaSearch.Forms
         private void BtnAutoUpdate_Click(object sender, EventArgs e)
         {
             AutoUpdater.Start("https://inazumaapps.info/inazumasearch/AutoUpdaterTest.xml");
+
+
+        }
+
+        
+
+        private void AutoUpdaterOnCheckForUpdateEvent(UpdateInfoEventArgs args)
+        {
+            if (AutoUpdater.DownloadUpdate(args))
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+            return;
+
+            if (args != null)
+            {
+                AutoUpdater.ShowUpdateForm(args);
+                if (args.IsUpdateAvailable)
+                {
+                    DialogResult dialogResult;
+                    if (args.Mandatory.Value)
+                    {
+                        dialogResult =
+                            MessageBox.Show(
+                                $@"There is new version {args.CurrentVersion} available. You are using version {args.InstalledVersion}. This is required update. Press Ok to begin updating the application.", @"Update Available",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        dialogResult =
+                            MessageBox.Show(
+                                $@"There is new version {args.CurrentVersion} available. You are using version {
+                                        args.InstalledVersion
+                                    }. Do you want to update the application now?", @"Update Available",
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Information);
+                    }
+
+                    // Uncomment the following line if you want to show standard update dialog instead.
+                    // AutoUpdater.ShowUpdateForm(args);
+
+                    if (dialogResult.Equals(DialogResult.Yes) || dialogResult.Equals(DialogResult.OK))
+                    {
+                        try
+                        {
+                            if (AutoUpdater.DownloadUpdate(args))
+                            {
+                                System.Windows.Forms.Application.Exit();
+                            }
+                        }
+                        catch (Exception exception)
+                        {
+                            MessageBox.Show(exception.Message, exception.GetType().ToString(), MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(@"There is no update available please try again later.", @"No update available",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show(
+                        @"There is a problem reaching update server please check your internet connection and try again later.",
+                        @"Update check failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
