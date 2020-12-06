75d89c3e4eb378d6a946108096f6b00cf70d1246
Dice <tetradice@gmail.com>
2020-12-07 05:40:44 +0900

    クロールダイアログの細かい挙動を調整

--- InazumaSearch/html/inazuma.js
+++ InazumaSearch/html/inazuma.js
@@ -330,10 +330,34 @@ function addFolderToCrawlModal(path, label, file_count) {
     $newItem.show();
 }
 
+
+// クロールダイアログ内、「全選択」チェックの表示更新
+function refreshCrawlModalAllCheck() {
+    // 全フォルダを選択していれば、「全選択」のチェックをON
+    if ($('.crawl-modal-folder-check[data-path]').not(':checked').length === 0) {
+        $('#CRAWL-MODAL-ALL-CHECK').prop('checked', true);
+    } else {
+        $('#CRAWL-MODAL-ALL-CHECK').prop('checked', false);
+    }
+}
+
+// クロール実行ボタンの表示更新
+function refreshCrawlDecideButtonEnabled() {
+    // 1つ以上のフォルダを選択していればクロール実行可能
+    if ($('.crawl-modal-folder-check[data-path]:checked').length >= 1) {
+        $('#CRAWL-MODAL-DECIDE').removeClass('disabled');
+    } else {
+        $('#CRAWL-MODAL-DECIDE').addClass('disabled');
+    }
+};
+
 // クロール実行前モーダルの対象フォルダ一覧を更新 (非同期に処理を行う)
 function updateFolderListOnCrawlModalAsync() {
     $('#PROGRESS-BAR-IN-CRAWL-MODAL').show();
-    $('.folder-item:not(.cloning-base)').remove();
+    $('.folder-item:not(.cloning-base)').remove(); // 既存行の削除
+    refreshCrawlDecideButtonEnabled(); // クロール実行ボタンの表示を更新
+
+    // 対象フォルダ一覧取得
     asyncApi.searchTargetDirectories().then(function (json) {
         var data = JSON.parse(json);
         if (data) {
@@ -342,6 +366,9 @@ function updateFolderListOnCrawlModalAsync() {
             }
         }
         $('#PROGRESS-BAR-IN-CRAWL-MODAL').hide();
+
+        refreshCrawlModalAllCheck(); // 「全選択」チェックの表示を更新
+        refreshCrawlDecideButtonEnabled(); // クロール実行ボタンの表示を更新
     });
 }
 
@@ -544,35 +571,19 @@ $(function(){
         }
     });
 
-    // クロール実行ボタンの表示更新
-    var refreshCrawlDecideButtonEnabled = function () {
-        // 1つ以上のフォルダを選択していればクロール実行可能
-        if ($('.crawl-modal-folder-check:visible:checked').length >= 1) {
-            $('#CRAWL-MODAL-DECIDE').removeClass('disabled');
-        } else {
-            $('#CRAWL-MODAL-DECIDE').addClass('disabled');
-        }
-    };
-
     // クロールダイアログで「全選択」ボタンを押下
     $('#CRAWL-MODAL-ALL-CHECK').click(function (e) {
         if ($(this).prop('checked')) {
-            $('.crawl-modal-folder-check:visible').prop('checked', true);
+            $('.crawl-modal-folder-check[data-path]').prop('checked', true);
         } else {
-            $('.crawl-modal-folder-check:visible').prop('checked', false);
+            $('.crawl-modal-folder-check[data-path]').prop('checked', false);
         }
 
         refreshCrawlDecideButtonEnabled(); // クロール実行ボタンの表示を更新
     });
     // クロールダイアログで、各フォルダのチェックボックスを変更
     $('#CRAWL-MODAL').on('click', '.crawl-modal-folder-check', function (e) {
-        // 全てチェックONであれば、「全選択」のチェックをON
-        if ($('.crawl-modal-folder-check:visible').not(':checked').length === 0) {
-            $('#CRAWL-MODAL-ALL-CHECK').prop('checked', true);
-        } else {
-            $('#CRAWL-MODAL-ALL-CHECK').prop('checked', false);
-        }
-
+        refreshCrawlModalAllCheck(); // 「全選択」チェックの表示を更新
         refreshCrawlDecideButtonEnabled(); // クロール実行ボタンの表示を更新
     });
 
