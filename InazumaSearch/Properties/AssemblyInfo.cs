744222f695030edeec050446f82e3abaad33b953
Dice <tetradice@gmail.com>
2021-07-31 09:52:32 +0900

    -> 0.21.0

--- InazumaSearch/Properties/AssemblyInfo.cs
+++ InazumaSearch/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Inazuma Search")]
-[assembly: AssemblyCopyright("Copyright ©  2017-2020")]
+[assembly: AssemblyCopyright("Copyright ©  2017-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.20.2.0")]
-[assembly: AssemblyFileVersion("0.20.2.0")]
+[assembly: AssemblyVersion("0.21.0.0")]
+[assembly: AssemblyFileVersion("0.21.0.0")]
