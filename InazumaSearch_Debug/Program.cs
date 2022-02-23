65530a731820586958d8dcf54c58c8c5fd4269e8
Dice <tetradice@gmail.com>
2022-02-24 01:32:12 +0900

    InazumaSearch_Debug のProgramクラスの実装を、InazumaSearch側と統一

--- InazumaSearch_Debug/Program.cs
+++ InazumaSearch_Debug/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using InazumaSearch.Forms;
 
 namespace InazumaSearch_Debug
 {
@@ -10,6 +11,10 @@ namespace InazumaSearch_Debug
         [STAThread]
         private static void Main(string[] args)
         {
+            // 例外ハンドラ登録
+            System.Windows.Forms.Application.ThreadException += Application_ThreadException;
+            System.Windows.Forms.Application.SetUnhandledExceptionMode(System.Windows.Forms.UnhandledExceptionMode.CatchException);
+            System.AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
             System.Windows.Forms.Application.EnableVisualStyles();
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
@@ -33,6 +38,24 @@ namespace InazumaSearch_Debug
             ));
         }
 
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            // システムエラーダイアログ表示
+            var f = new SystemErrorDialog(e.Exception);
+            f.ShowDialog();
+            System.Windows.Forms.Application.Exit();
+        }
 
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = (Exception)e.ExceptionObject;
+
+            // システムエラーダイアログ表示
+            var f = new SystemErrorDialog(ex);
+            f.ShowDialog();
+            System.Windows.Forms.Application.Exit();
+        }
     }
+
+
 }
