d51a2c023d0c27d356fe7984eddfb594f4230245
Dice <tetradice@gmail.com>
2022-09-08 23:06:27 +0900

    クロール対象フォルダ選択画面で、フォルダラベルが表示されるようにした

--- InazumaSearch/html/inazuma.js
+++ InazumaSearch/html/inazuma.js
@@ -320,6 +320,7 @@ function addFolderToCrawlModal(path, label, fileCount, checked) {
     $newItem.find('.crawl-modal-folder-check').attr('data-path', path).prop('checked', checked);
     $newItem.find('.path').text(path);
     $newItem.find('.file-count').text(fileCount);
+    $newItem.find('.folder-label').text(label);
 
     $('#CRAWL-FOLDER-LIST li.folder-item:last').after($newItem);
     $newItem.show();
